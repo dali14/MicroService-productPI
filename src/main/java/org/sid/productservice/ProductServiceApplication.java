3af85b31a7cb3e3b9e0ee32c971f755024fa6c6d
dali14 <dalijlasii@gmail.com>
2022-12-02 00:05:54 +0100

    CommandLineRunner

--- src/main/java/org/sid/productservice/ProductServiceApplication.java
+++ src/main/java/org/sid/productservice/ProductServiceApplication.java
@@ -1,9 +1,13 @@
 package org.sid.productservice;
 
+import org.sid.productservice.entities.Product;
+import org.sid.productservice.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -13,9 +17,18 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner (){
+	public CommandLineRunner commandLineRunner (ProductRepository productRepository){
 		return args -> {
 
+			productRepository.saveAll(
+					List.of(
+							Product.builder().name("pen").price(5.0).image("Link1").build(),
+							Product.builder().name("basket").price(15.5).image("Link2").build(),
+							Product.builder().name("book").price(3.3).image("Link3").build()
+
+					)
+
+			);
 		};
 	}
 
