3d8fc0dec43bcd36b9ccfdcd4d99baa7b5612799
dali14 <dalijlasii@gmail.com>
2022-12-01 23:59:27 +0100

    package repository

--- src/main/java/org/sid/productservice/ProductServiceApplication.java
+++ src/main/java/org/sid/productservice/ProductServiceApplication.java
@@ -1,13 +1,22 @@
 package org.sid.productservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner (){
+		return args -> {
+
+		};
 	}
 
 }
