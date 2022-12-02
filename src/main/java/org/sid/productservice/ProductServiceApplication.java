ec74f65b225590d950faed42c8b356bc01a3745a
dali14 <dalijlasii@gmail.com>
2022-12-02 01:03:20 +0100

    exposeIdsFor product class

--- src/main/java/org/sid/productservice/ProductServiceApplication.java
+++ src/main/java/org/sid/productservice/ProductServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.List;
 
@@ -17,8 +18,9 @@ public class ProductServiceApplication {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner (ProductRepository productRepository){
+	public CommandLineRunner commandLineRunner (ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
 		return args -> {
+			restConfiguration.exposeIdsFor(Product.class);
 
 			productRepository.saveAll(
 					List.of(
@@ -29,6 +31,9 @@ public class ProductServiceApplication {
 					)
 
 			);
+			productRepository.findAll().forEach(p->{
+				System.out.println(p);
+			});
 		};
 	}
 
